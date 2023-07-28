6c1cea9bc2c425d438e865405aa40798889a627b
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-07-28 08:33:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+//===============================JS CLASS 2====================================//
+
+
+
+//====================================String Methods=====================================//
+//(1)CharAt => To find out the character at a location.
+//(2)Split => To split the parts of string. 
+//(3)toString => To convert any type of data type in to string.
+//(4)typeOf => To know the datatype of a value.
+//(5)parsIent => To convert the string in to number.
+//(6)Number => To convert the string datatype in to number including string.
+//(7)toUppperCase and toLowerCase => To uppercase ans lowercase a  string.
+//(8)(startsWith) => To know that if the character is found on the declared index.
+
+//====(Examples)====//
+//(chartAt):-
+// const myName = "asma";
+// console.log(myName.charAt(0));
+
+//(split):-
+// const country = "Pakistan";
+// console.log(country.split(""));
+
+//(toString):-
+// const number = 123456;
+// const newNumber = number.toString();
+// console.log(typeof(newNumber));
+
+//(typeOf):-
+// const age = 17;
+// console.log(typeof(age));
+
+//(parseInt):-
+// const myAge = "17";
+// const newOne = parseInt(myAge);
+// console.log(typeof(newOne));
+
+//(Number):-
+// const myRollNumber = "112";
+// console.log(typeof(myRollNumber))
+// const string = Number(myRollNumber);
+// console.log(typeof(string));
+
+//(toUpperCase and toLowerCase):-
+// var mySisterName = "muqadas";
+// var firstChar = mySisterName.charAt(0);
+// firstChar = firstChar.toUpperCase();
+// var otherChars = mySisterName.slice(1);
+// otherChars = otherChars.toLowerCase();
+// var capped = firstChar + otherChars;
+// console.log(capped);
+
+//(startsWith):-
+// const numbers = 12345;
+// const updated = numbers.toString()
+// if (updated.startsWith("1")) {
+// console.log(true);
+// }
+// else {
+// console.log(false);
+// }
+
+//(Includes):-
+// const text = "asma khan from karachi";
+// if (text.includes("khan")) {
+// console.log("a is in var string");
+// } else { 
+// console.log("a is in not var string");
+// }
+
+
+
+
+
+
+
+
